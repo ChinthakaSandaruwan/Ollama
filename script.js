2540a092919216f46aae45c889c3b7affadaf77c
ChinthakaSandaruwan <chinthakasw000@gmail.com>
2026-04-09 21:25:44 +0530

    Add initial HTML and JavaScript files for Ollama project

---
+++ script.js
@@ -0,0 +1,20 @@
+async function test() {
+    let prompt = document.getElementById("x").value;
+    console.log(prompt);
+
+    let url = "http://localhost:11434/api/generate";
+    let data = {
+        model: "qwen3.5:0.8b",
+        prompt: "why is the sky blue?",
+    };
+
+    let request = await fetch(url);
+
+}
+
+
+
+// curl http://localhost:11434/api/generate -d '{
+//   "model": "gemma3",
+//   "prompt": "Why is the sky blue?"
+// }'
