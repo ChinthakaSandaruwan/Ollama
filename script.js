e71a8eb76ba0acc65d3cc7231db5bd3832ccae31
ChinthakaSandaruwan <chinthakasw000@gmail.com>
2026-04-09 22:36:55 +0530

    Rename test function to AI and update button onclick handler; modify fetch request to use dynamic prompt input

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-async function test() {
+async function AI() {
+
     let prompt = document.getElementById("x").value;
+
     console.log(prompt);
 
     let url = "http://localhost:11434/api/generate";
     let data = {
         model: "qwen3.5:0.8b",
-        prompt: "why is the sky blue?",
+        prompt: prompt,
+        stream: false
     };
 
     let request = await fetch(
         url,
         {
             method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
             body: JSON.stringify(data),
+
         }
     );
 
-}
+    let result = await request.json();
+    
+    document.getElementById("output").innerText = result.response;
+
+}   
